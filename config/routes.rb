d6249cc80559b6bca0b43b7b5b86cdb13c54cf67
JMD60260 <dardy.jeanmarc@gmail.com>
2019-09-06 17:20:20 +0200

    create README and bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'staticpage/new'
   root'staticpage#index'
   get 'staticpage/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
