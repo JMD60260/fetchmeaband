3ebf1e9a8704581b953e5e3aa252e64fb857665e
JMD60260 <dardy.jeanmarc@gmail.com>
2019-09-09 18:31:25 +0200

    create user mailer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   get 'staticpage/new'
-  root'staticpage#index'
+  root 'staticpage#index'
   get 'staticpage/show'
   resources :city, only: [:show]
   resources :forum do
