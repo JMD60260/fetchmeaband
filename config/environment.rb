240dbf349a36ca474800368505b161645b14ae4f
JMD60260 <dardy.jeanmarc@gmail.com>
2019-09-11 19:15:55 +0200

    change SENDGRID2

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://fetchmeaband.herokuapp.com',
+    :domain => 'https://fetchmeaband-staging.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
