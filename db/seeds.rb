687a361ebbf5c833cf1ddd89a0c497d30505b1f2
jb1011 <demenet.jb@hotmail.fr>
2019-09-11 18:21:38 +0200

    html update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do 
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    age: rand(18..88),
+    password: "password"
+    )
+end
