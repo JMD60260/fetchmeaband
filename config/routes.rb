068a8f9fe285677441e0f080278d67863dc39e23
JMD60260 <dardy.jeanmarc@gmail.com>
2019-09-12 16:06:13 +0200

    try fixed 3

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
   resources :comments
   resources :staticpage
-  root 'staticpage#new'
+  root 'staticpage#index'
   
   resources :city, only: [:show]
   resources :forum do
