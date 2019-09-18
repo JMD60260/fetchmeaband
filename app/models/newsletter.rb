41b7e80a63b1b5890f19c9ce34145e694ef6daf6
JMD60260 <dardy.jeanmarc@gmail.com>
2019-09-18 23:05:24 +0200

    create newsletter model

---
+++ app/models/newsletter.rb
@@ -0,0 +1,2 @@
+class Newsletter < ApplicationRecord
+validates :name, presence: true validates :email, presence: true end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_104107) do
+ActiveRecord::Schema.define(version: 2019_09_18_205827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,13 @@ ActiveRecord::Schema.define(version: 2019_09_17_104107) do
     t.string "message_id"
     t.index ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
     t.index ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
+  end
+
+  create_table "newsletters", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
