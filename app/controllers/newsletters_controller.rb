4bedacabb5d377abddc56be25d3b1508781464a3
codefactor-io <support@codefactor.io>
2019-09-19 10:37:34 +0000

    [CodeFactor] Apply fixes

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,10 +1,10 @@
 class NewslettersController < ApplicationController
-  
+
     before_action :set_newsletter, only: [:destroy]
 
     def create
       @newsletter = Newsletter.new(newsletter_params)
-      if @newsletter.save 
+      if @newsletter.save
         cookies[:save_lead] = true
         redirect_to root_path, notice: "Demande prise en compte."
       else
