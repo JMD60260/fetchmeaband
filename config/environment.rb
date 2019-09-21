dd29f30ca7557fab0be71032959a9f164a8f058a
JMD60260 <dardy.jeanmarc@gmail.com>
2019-09-21 13:26:40 +0200

    change environement.rb date french format

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+my_date_formats = { :default => '%d/%m/%Y' } 
+Time::DATE_FORMATS.merge!(my_date_formats) 
+Date::DATE_FORMATS.merge!(my_date_formats)
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
