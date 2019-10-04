f4a8f0f490fce957484eb27b818c5bca3ee65cd3
jb1011 <demenet.jb@hotmail.fr>
2019-10-04 12:05:52 +0200

    try

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Fetchmeaband
     config.load_defaults 5.2
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :fr
+    config.secret_key_base="f2fcfbb9f1ce7a34a8634fa3b9bc890e79d523526cce90b82f4c3a8a6717c2cdda7f8ecd6858aa1058ae9024b84fa0006cd1b30f86dd10bab7bd7844556fa570"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
